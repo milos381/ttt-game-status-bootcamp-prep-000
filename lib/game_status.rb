ef468d4cb326d4b9f2b2068ca87b4fa6849aa1bc
Milos Popovic <milos_popovic@outlook.com>
2020-05-20 16:29:14 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -24,10 +24,6 @@ def won?(board)
       return win_combination
     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
       return win_combination
-    else position_taken?(board, index)
-      return nil
-    else draw?(board)
-      return nil
     end
   end
 end
